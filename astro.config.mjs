03a13f44abfd2f0c86b3ad72481ebcc0428b7a43
FugitiveDock282 <moonstarer@tuta.io>
2026-09-14 17:36:16 +0200

    Made compatible with hosting on Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://fugitivedock282.github.io',
+  base: '/portfolio/',
   build: {
     format: 'file',
   },
